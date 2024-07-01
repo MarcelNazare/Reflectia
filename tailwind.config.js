a15bed2a3891db947aa11f5739b580683b717be4
Marcel Nazare <realmarcelnazare@gmail.com>
2024-07-01 17:04:15 +0200

    still working on way to change the tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,23 @@ module.exports = {
     './templates/**/*.html'
     ],
   theme: {
+    colors :{
+      'deep-koamaru': {
+        '50': '#f2f5ff',
+        '100': '#e8eeff',
+        '200': '#d4dfff',
+        '300': '#b1c2ff',
+        '400': '#8499ff',
+        '500': '#5369ff',
+        '600': '#2f3bf8',
+        '700': '#1d24e4',
+        '800': '#181fbf',
+        '900': '#161c9c',
+        '950': '#0c1475',
+    },
+    
+    },
+    },
     extend: {
       fontFamily:{
         sans:['Inter var', ...defaultTheme.fontFamily.sans],
@@ -17,16 +34,11 @@ module.exports = {
         md: '768px',
         lg: '1024px',
         xl: '1280px',       
-      }
+      },
     },
-  },
   plugins: [
     require('@tailwindcss/typography'),
     require('daisyui'),
     typography,
   ],
 }
-
-
-
-
