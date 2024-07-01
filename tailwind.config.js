6dfa211e80b14e198d726ab77791bcddff796c97
Marcel Nazare <realmarcelnazare@gmail.com>
2024-07-01 14:02:46 +0200

    changed the themes font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,32 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+const typography = require('@tailwindcss/typography')
+
 module.exports = {
   content: [
     './templates/**/*.html'
     ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        sans:['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      screens:{
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',       
+      }
+    },
   },
   plugins: [
+    require('@tailwindcss/typography'),
     require('daisyui'),
+    typography,
   ],
 }
+
+
+
 
